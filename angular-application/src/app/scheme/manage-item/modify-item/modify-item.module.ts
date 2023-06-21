087ef04244891b0fe6b6b7f981d42e4e96afbf81
jagadishdandpat <jagdishdandpat2@gmail.com>
2023-06-21 12:54:42 +0530

    changes in modify Item module

--- angular-application/src/app/scheme/manage-item/modify-item/modify-item.module.ts
+++ angular-application/src/app/scheme/manage-item/modify-item/modify-item.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ModifyItemRoutingModule } from './modify-item-routing.module';
 import { ModifyItemComponent } from './modify-item/modify-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ModifyItemRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class ModifyItemModule { }
